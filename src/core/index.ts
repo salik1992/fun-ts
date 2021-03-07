63e77423a9b10dabed4c71b43071e15eeb48259f
Vít Šalomon <vit@salomon.cloud>
2021-03-07 13:40:38 +0100

    feat(core): added basic math functions

---
+++ src/core/index.ts
@@ -0,0 +1,6 @@
+// MARK: MATH
+export { add } from './add';
+export { subtract } from './subtract';
+export { multiply } from './multiply';
+export { divide } from './divide';
+export { modulo } from './modulo';
