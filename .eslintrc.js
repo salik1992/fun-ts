063a90571e5e9e3bd52c9ea1d2e6f277d2301601
Vít Šalomon <vit@salomon.cloud>
2021-03-07 18:39:09 +0100

    chore: added more configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
-   extends: ['airbnb-typescript'],
-   parserOptions: {
-      project: './tsconfig.json',
-   },
-   rules: {
-      "import/prefer-default-export": "off",
-   },
- };
+    extends: ['airbnb-typescript'],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    rules: {
+        "indent": "off",
+        "@typescript-eslint/indent": ["error", 4],
+        "@typescript-eslint/no-unused-vars": "off",
+        "import/prefer-default-export": "off",
+    },
+};
