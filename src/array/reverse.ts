2fbe5d934b4a416ca2119d6e2f4aada3bb2ae919
Vít Šalomon <vit@salomon.cloud>
2021-03-26 20:41:07 +0100

    fix(array): made reverse with builtin function above shallow copy

--- src/array/reverse.ts
+++ src/array/reverse.ts
@@ -1,3 +1,1 @@
-import { map } from './map';
-
-export const reverse = <T>(array: T[]) => map((_, index) => array[array.length - 1 - index]);
+export const reverse = <T>(array: T[]) => [...array].reverse();
