6bb11f0d6216a3be974c9d1b86a5f892f32390b9
Vít Šalomon <vit@salomon.cloud>
2021-04-03 18:45:56 +0200

    fix(array): fixed type failing tests

--- src/array/__tests__/find.spec.ts
+++ src/array/__tests__/find.spec.ts
@@ -2,10 +2,10 @@ import { find } from '../find';
 
 describe('find', () => {
     it('should return the first item that matches the predicate', () => {
-        expect(find((n) => n > 2)([1, 2, 3, 4, 5])).toBe(3);
+        expect(find((n: number) => n > 2)([1, 2, 3, 4, 5])).toBe(3);
     });
 
     it('should return undefined if nothing matches the predicate', () => {
-        expect(find((n) => n > 5)([1, 2, 3, 4, 5])).toBe(undefined);
+        expect(find((n: number) => n > 5)([1, 2, 3, 4, 5])).toBe(undefined);
     });
 });
--- src/array/__tests__/findIndex.spec.ts
+++ src/array/__tests__/findIndex.spec.ts
@@ -2,10 +2,10 @@ import { findIndex } from '../findIndex';
 
 describe('findIndex', () => {
     it('should return the index of the first item that matches the predicate', () => {
-        expect(findIndex((n) => n > 2)([1, 2, 3, 4, 5])).toBe(2);
+        expect(findIndex((n: number) => n > 2)([1, 2, 3, 4, 5])).toBe(2);
     });
 
     it('should return -1 if nothing matches the predicate', () => {
-        expect(findIndex((n) => n > 5)([1, 2, 3, 4, 5])).toBe(-1);
+        expect(findIndex((n: number) => n > 5)([1, 2, 3, 4, 5])).toBe(-1);
     });
 });
