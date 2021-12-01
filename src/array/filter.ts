5b49604e2fef973f36ffda5b39d1fee4673d9636
Vít Šalomon <vit@salomon.cloud>
2021-12-01 12:05:41 +0100

    feat(array): added optional retyping for the result of filter

--- src/array/filter.ts
+++ src/array/filter.ts
@@ -1,3 +1,3 @@
-export const filter = <T>(
+export const filter = <T, U extends T = T>(
     predicate: (value: T, index: number, array: T[]) => boolean,
-) => (array: T[]) => array.filter(predicate);
+) => (array: T[]) => array.filter(predicate) as U[];
