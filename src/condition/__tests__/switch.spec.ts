a006fee41e21ccaaa1b186f70664fa3a708c2d2e
Vít Šalomon <vit@salomon.cloud>
2021-12-03 09:39:16 +0100

    refactor(condition)!: switches more like switch

--- src/condition/__tests__/switch.spec.ts
+++ src/condition/__tests__/switch.spec.ts
@@ -1,27 +1,25 @@
 import { switches, switchCase, defaultCase } from '../switch';
 
 describe('switch', () => {
-    it('should return the result of defaultCase if no condition matches', () => {
-        const a = 1 as number;
-        const b = 2 as number;
-        expect(switches(
-            switchCase(a === b)(() => 1),
-            switchCase(a > b)(() => 2),
+    it('should return the result of defaultCase if no value matches', () => {
+        const a = 1;
+        expect(switches(a)(
+            switchCase(0)(() => 1),
+            switchCase(2)(() => 2),
             defaultCase(() => 3),
         )).toBe(3);
     });
 
-    it('should return undefined if no condition matches', () => {
-        const a = 1 as number;
-        const b = 2 as number;
-        expect(switches(
-            switchCase(a === b)(() => 1),
-            switchCase(a > b)(() => 2),
+    it('should return undefined if no value matches', () => {
+        const a = 1;
+        expect(switches(a)(
+            switchCase(0)(() => 1),
+            switchCase(2)(() => 2),
         )).toBe(undefined);
     });
 
-    it('should return the first condition that matches', () => {
-        expect(switches(
+    it('should return the first value that matches', () => {
+        expect(switches(true)(
             switchCase(true)(() => 1),
             switchCase(true)(() => 2),
         )).toBe(1);
--- src/condition/switch.ts
+++ src/condition/switch.ts
@@ -1,54 +1,44 @@
 import { cond } from './cond';
 
-type Case<T> = {
-    condition: boolean,
-    branch: () => T,
+type Case<T, U> = {
+    value: T,
+    branch: () => U,
+    isDefaultCase?: true,
 };
 
-type GetReturnValue<
-    FirstCase extends Case<any>,
-    OtherCases extends Case<any>[],
-> = FirstCase['condition'] extends true
-    ? ReturnType<FirstCase['branch']>
-    : OtherCases extends [infer NextCase, ...infer NextOtherCases]
-        ? FirstCase['condition'] extends false
-            ? NextCase extends Case<any>
-                ? NextOtherCases extends Case<any>[]
-                    ? GetReturnValue<NextCase, NextOtherCases>
-                    : GetReturnValue<NextCase, []>
-                : undefined
-            : ReturnType<FirstCase['branch']> | (
-                NextCase extends Case<any>
-                    ? NextOtherCases extends Case<any>[]
-                        ? GetReturnValue<NextCase, NextOtherCases>
-                        : GetReturnValue<NextCase, []>
-                    : undefined
-            )
-        : FirstCase['condition'] extends false
-            ? undefined
-            : ReturnType<FirstCase['branch']> | undefined;
+type GetReturnType<
+    FirstCase extends Case<any, any>,
+    OtherCases extends Case<any, any>[],
+> = () => OtherCases extends [infer NextCase, ...infer NextOtherCases]
+    ? NextCase extends Case<any, any>
+        ? NextOtherCases extends Case<any, any>[]
+            ? ReturnType<FirstCase['branch']> | GetReturnType<NextCase, NextOtherCases>
+            : ReturnType<FirstCase['branch']> | ReturnType<NextCase['branch']>
+        : ReturnType<FirstCase['branch']>
+    : ReturnType<FirstCase['branch']>;
 
-export const switches = <
-    FirstCase extends Case<any>,
-    OtherCases extends Case<any>[],
-    ReturnValue extends GetReturnValue<FirstCase, OtherCases>,
->({ condition, branch }: FirstCase, ...cases: OtherCases): ReturnValue => (
-    cond(condition)(
+export const switches = <T>(testedValue: T) => <
+    FirstCase extends Case<T, any>,
+    OtherCases extends Case<T, any>[],
+>(
+    { value, branch, isDefaultCase }: FirstCase, ...cases: OtherCases
+): GetReturnType<FirstCase, OtherCases> => (
+    cond(isDefaultCase || testedValue === value)(
         branch,
     )(
         () => cond(cases.length > 0)(
             // @ts-ignore - TS cannot count parameters
-            () => switches(...cases),
+            () => switches(testedValue)(...cases),
         )(
             () => undefined,
         ),
     )
 );
 
-export const switchCase = (condition: boolean) => <T>(branch: () => T): Case<T> => ({
-    condition, branch,
+export const switchCase = <T>(value: T) => <U>(branch: () => U): Case<T, U> => ({
+    value, branch,
 });
 
-export const defaultCase = <T>(branch: () => T): Case<T> => ({
-    condition: true, branch,
+export const defaultCase = <T>(branch: () => T): Case<any, T> => ({
+    value: undefined, branch, isDefaultCase: true,
 });
